79b8a9d84ee530431994046af3f3ab40d8a8b294
Rivridis <sanjaypugal.personal@gmail.com>
2023-10-15 19:34:44 +0300

    reader logic

--- ui_main.py
+++ ui_main.py
@@ -1,9 +1,10 @@
-from PySide6.QtWidgets import QApplication, QDialog, QVBoxLayout,QMessageBox,QLineEdit,QPushButton,QMainWindow,QSplashScreen,QFileDialog,QTreeView,QFileSystemModel
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication, QDialog, QVBoxLayout,QMessageBox,QLineEdit,QPushButton,QMainWindow,QSplashScreen,QFileDialog,QTreeView,QFileSystemModel,QTextBrowser,QTextEdit
+from PySide6.QtGui import QPixmap,QTextCharFormat,QFont,QTextCursor,QColor
 from PySide6.QtCore import Qt
 from PySide6.QtUiTools import QUiLoader
 import sys
-
+from ebooklib import epub
+from bs4 import BeautifulSoup
 
 import time
 
@@ -40,12 +41,17 @@ class Main(QMainWindow):
         self.directory = ui.findChild(QPushButton,"direct")
         self.directory.clicked.connect(self.dirpop)
 
-        self.tree = ui.findChild(QTreeView,"tree")
+        self.tree = ui.findChild(QTreeView,"tree") 
 
         self.model = QFileSystemModel()
         self.model.setRootPath("/")
         self.tree.setModel(self.model)
         self.tree.setRootIndex(self.model.index("/"))
+
+        self.tree.doubleClicked.connect(self.choose)
+
+        self.text = ui.findChild(QTextEdit,'text')
+        self.text.setReadOnly(True)
 
 
     def dirpop(self):
@@ -54,10 +60,44 @@ class Main(QMainWindow):
 
         if directory:
             self.tree.setRootIndex(self.model.index(directory))
+    
+    def choose(self,index):
+        file_path = self.model.filePath(index)
+        if ".epub" in file_path:
+            book = epub.read_epub(file_path)
+            content = ""
+            for item in book.items:
+                if isinstance(item, epub.EpubHtml):
+                    content += item.content.decode('utf-8')
+            self.text.setHtml(content)
+
+            #text_format = QTextCharFormat()
+
+
+            # # Set font color
+            # text_format.setForeground(QColor("#FFFFFF"))  # Change to desired color
+
+            # # Set font family
+            # font = QFont("garamond")  # Change to desired font family
+            # text_format.setFont(font)
+
+            # # Set font size
+            # text_format.setFontPointSize(22)  # Change to desired size
+
+            # # Apply the format to the entire document
+            # cursor = self.text.textCursor()
+            # cursor.select(QTextCursor.Document)
+            # cursor.setCharFormat(text_format)
+            
+        
+        else:
+            QMessageBox.information(self, "Invalid filetype","Please choose another file")
+        
 
 
 if __name__ == "__main__":
     main()
+    
     
 
 
