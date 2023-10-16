a29a0a5aaead927c7c405a24ee5f946bccca31b0
Rivridis <sanjaypugal.personal@gmail.com>
2023-10-17 00:06:13 +0300

    IT FINALLY WORKS AHH

--- ui_main.py
+++ ui_main.py
@@ -1,14 +1,14 @@
 from PySide6.QtWidgets import QApplication, QDialog, QVBoxLayout,QMessageBox,QLineEdit,QPushButton,QMainWindow,QSplashScreen,QFileDialog,QTreeView,QFileSystemModel,QTextBrowser,QTextEdit
 from PySide6.QtGui import QPixmap,QTextCharFormat,QFont,QTextCursor,QColor
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt,QTimer,QPoint
 from PySide6.QtUiTools import QUiLoader
 import sys
 from ebooklib import epub
 from bs4 import BeautifulSoup
 import qdarktheme
-
+import hashlib
 import time
-
+scroll_positions = {}
 qss = """
         QScrollBar::sub-page:horizontal {
         background: rgba(255, 255, 255, 0)
@@ -83,7 +83,6 @@ def main():
 class Main(QMainWindow):
     def __init__(self):
         super().__init__()
-        
         loader = QUiLoader()
         ui_file = "assets/main.ui" 
         ui = loader.load(ui_file, self)
@@ -111,10 +110,12 @@ class Main(QMainWindow):
         if directory:
             self.tree.setRootIndex(self.model.index(directory))
     
-    def choose(self,index):
-        file_path = self.model.filePath(index)
-        if ".epub" in file_path:
-            book = epub.read_epub(file_path)
+    
+    def choose(self, index):
+        self.file_path = self.model.filePath(index)
+        if ".epub" in self.file_path:
+
+            book = epub.read_epub(self.file_path)
             content = ""
             for item in book.items:
                 if isinstance(item, epub.EpubHtml):
@@ -125,9 +126,9 @@ class Main(QMainWindow):
                     <style>
                         body 
                         {{
-                            color: rgb(255,255,255);
+                            color: rgb(255, 255, 255);
                             font-size: 16px;
-                            background-color: rgba(26,26,29,1);
+                            background-color: rgba(26, 26, 29, 1);
                         }}
                     </style>
                 </head>
@@ -137,10 +138,32 @@ class Main(QMainWindow):
             </html>
             """
             self.text.setHtml(styled_content)
+
+            # Restore the scroll position if available
+            if self.file_path in scroll_positions:
+                text_cursor = self.text.textCursor()
+                text_cursor.setPosition(scroll_positions[self.file_path])
+                self.text.setTextCursor(text_cursor)
+            else:
+                scroll_positions[self.file_path] = 0
+
+            print(scroll_positions)
+            self.autosaveTimer = QTimer()
+            self.autosaveTimer.timeout.connect(self.saveFile)
+            self.autosaveTimer.setInterval(1000)
+            self.autosaveTimer.start()
+
         else:
             QMessageBox.information(self, "Invalid filetype","Please choose another file")
-        
 
+    def saveFile(self):
+            cursorTop = self.text.cursorForPosition(QPoint(0, 0))
+            self.position = cursorTop.position()
+            if self.file_path in scroll_positions:
+                scroll_positions[self.file_path] = self.position
+            else:
+                scroll_positions[self.file_path] = 0
+            print(self.position)
 
 if __name__ == "__main__":
     main()
