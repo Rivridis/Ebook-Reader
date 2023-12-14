62683e719aa8f0f6a44869f297109fd50050384e
Rivridis <sanjaypugal.personal@gmail.com>
2023-12-15 00:52:14 +0300

    revert, everything went to shit

--- ui_main.py
+++ ui_main.py
@@ -1,4 +1,3 @@
-import PySide6
 from PySide6.QtWidgets import QApplication, QMessageBox,QPushButton,QMainWindow,QSplashScreen,QFileDialog,QTreeView,QFileSystemModel,QTextEdit
 from PySide6.QtGui import QPixmap,QStandardItem,QStandardItemModel
 from PySide6.QtCore import Qt,QTimer,QStandardPaths,QCoreApplication
@@ -6,7 +5,6 @@ from PySide6.QtUiTools import QUiLoader
 import sys
 import ebooklib
 from ebooklib import epub
-import qdarktheme
 import hashlib
 import time
 import json
@@ -19,51 +17,9 @@ except FileNotFoundError:
     # If the file doesn't exist yet, create an empty dictionary
     scroll_positions = {}
 
-qss = """
-        QScrollBar::sub-page:horizontal {
-        background: rgba(255, 255, 255, 0)
-    }
-
-    QScrollBar::add-page:horizontal {
-        background: rgba(255, 255, 255, 0)
-    }
-    QScrollBar::sub-page:vertical {
-        background: rgba(255, 255, 255, 0)
-    }
-
-    QScrollBar::add-page:vertical {
-        background: rgba(255, 255, 255, 0)
-    }
-    QScrollBar::handle
-    {
-        background : rgba(138,124,171,1);
-        min-height: 40px;
-    }
-    QScrollBar::handle:vertical
-    {
-        background : rgba(138,124,171,1);
-        min-height: 40px;
-    }
-    QScrollBar::handle::pressed
-    {
-    background : rgba(138,124,171,1); 
-    }
-    QScrollBar::handle::hover
-    {
-    background : rgba(138,124,171,1);
-    }
-    QScrollBar::add-line,
-    QScrollBar::sub-line {
-        width: 0;
-        subcontrol-position: right;
-        subcontrol-origin: margin;
-    }
-
-
-    """
 
 def show_splash_screen():
-    splash_pix = QPixmap('assets\splash.jpg')
+    splash_pix = QPixmap('assets\\splash.jpg')
     splash_pix = splash_pix.scaled(550, 450, Qt.KeepAspectRatio,Qt.SmoothTransformation)
     splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
     splash.setMask(splash_pix.mask())
@@ -73,14 +29,6 @@ def show_splash_screen():
 
 def main():
     app = QApplication(sys.argv)
-    qdarktheme.setup_theme(additional_qss=qss,custom_colors={
-        "[dark]": {
-            "primary": "#D0BCFF",
-            "border": "#00000000",
-            "treeSectionHeader.background":"#8a7cab"
-        }
-    })
-
     # Create and show splash screen
     splash = show_splash_screen()
     main_window = Main()
